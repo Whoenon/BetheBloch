a8e2f555b6a76f01c801598b325e8aa34f1078f5
Antonio Guastella <65413476+Whoneon@users.noreply.github.com>
2021-05-06 19:03:54 +0200

    Update BB.c

--- BB.c
+++ BB.c
@@ -1,3 +1,4 @@
+//Funzioni che ci serviranno man mano
 void BB();
 void exit_here();
 void particle();
@@ -6,17 +7,16 @@ double bethe_bloch();
 void draw_graph();
 void plot_dissipazione();
 
-
-
+//Puntatori relativi ad elementi grafici
 TCanvas *main_window;
 TButton *exit_button; 
 TButton *alpha_button;
 TButton *proton_button;
 TButton *muon_button;
 TButton *graph_button;
-TButton *gold_button;
+TButton *au_button;
+TButton *ag_button;
 TButton *al_button;
-TButton *alluminio_button;
 TButton *fe_button;
 TButton *dissipazione_button;
 TButton *clear_button;
@@ -28,40 +28,32 @@ double m; //massa del proiettile
 int Z; //numero atomico del bersaglio
 float A; //massa atomica relativa
 float z_particle; //numero atomico proiettile
-double mrel; //
+double mrel; //massa relativa del proiettile rispetto a quella dell'elettrone
 
 //Inizializzo e dichiaro le costanti globali
 double c = 2.9979*pow(10,8); //velocità luce in m/s
 double me = 0.511*pow(10,6); //massa a riposo elettrone in eV/c^2
-int e = 1; //carica elettrone in eV
 double K = 0.307075*pow(10,6); //costante K
-long int incrementi = 10000; //step di integrazione e di calcolo dell'equazione
-float betagamma_max = 10000; 
-float betagamma_min = 0.1;
-float TEXT_SIZE=0.30;
-float MAX_SPESSORE = 1000;
 
-//valori fissi per l'interfaccia grafica
-/*Double_t w = 1250;
-Double_t h = 800;
-*/
-void BB() 
+//Inizializzo e dichiaro le costanti selezionabili dall'utente
+long int INCREMENTI = 10000; //step di calcolo dell'equazione
+float BETAGAMMA_MAX = 10000; //valore massimo di beta*gamma che compare nel grafico di Bethe-Bloch
+float BETAGAMMA_MIN = 0.1; //valore minimo
+float TEXT_SIZE=0.30; //grandezza del font dei bottoni
+float INC = 0.001; //spessore infinitesimo in cm;
+
+void BB()  //Interfaccia utente
 {
 	TCanvas * main_window = new TCanvas("main_window","Bethe-Bloch");
 	TPad* pad1 = new TPad("pad1", "Graph", 0, 0, 0.9, 1);
 	TPad* pad2 = new TPad("pad2", "Buttons", 0.90, 0, 1, 1, 38);
 	pad1->Draw();
 	pad2->Draw();
-	/*
-	main_window->SetCanvasSize(w, h);
-	main_window->SetWindowSize(w, h);
-	*/
-	//Uso pad2 per organizzare i miei bottoni
+
+	//In pad2 andranno i bottoni
 	pad2->cd();
 	
-	//Creo dei bottoni che aiutino l'utente nella scelta della configurazione iniziale
-
-
+	//Creo dei bottoni che aiutino l'utente nella scelta del proiettile...
 	proton_button = new TButton("Protone", "particle(0)", 0, 0.95, 1, 0.90);
 	proton_button->SetTextSize(TEXT_SIZE);
 	proton_button->Draw();
@@ -74,27 +66,24 @@ void BB()
 	muon_button->SetTextSize(TEXT_SIZE);
 	muon_button->Draw();
 	
-	//Bottone "ferro"
+	//... e del bersaglio
 	fe_button = new TButton("Ferro", "atomic_number(0)", 0, 0.75, 1, 0.7);
 	fe_button->SetTextSize(TEXT_SIZE);
 	fe_button->Draw();
 
-	//Bottone "oro"
-	gold_button = new TButton("Oro", "atomic_number(1)", 0, 0.7, 1, 0.65);
-	gold_button->SetTextSize(TEXT_SIZE);
-	gold_button->Draw();
+	au_button = new TButton("Oro", "atomic_number(1)", 0, 0.7, 1, 0.65);
+	au_button->SetTextSize(TEXT_SIZE);
+	au_button->Draw();
 
-	//Bottone "argento"
-	al_button = new TButton("Argento", "atomic_number(2)", 0, 0.65, 1, 0.6);
+	ag_button = new TButton("Argento", "atomic_number(2)", 0, 0.65, 1, 0.6);
+	ag_button->SetTextSize(TEXT_SIZE);
+	ag_button->Draw();
+	
+	al_button = new TButton("Alluminio", "atomic_number(3)", 0, 0.6, 1, 0.55);
 	al_button->SetTextSize(TEXT_SIZE);
 	al_button->Draw();
-	
-	//bottone "alluminio"
-	alluminio_button = new TButton("Alluminio", "atomic_number(3)", 0, 0.6, 1, 0.55);
-	alluminio_button->SetTextSize(TEXT_SIZE);
-	alluminio_button->Draw();
 
-
+	//Bottoni che richiamano funzioni ed eseguono comandi:
 	graph_button = new TButton("Grafico Bethe-Bloch", "draw_graph()", 0, 0.4,1, 0.35);
 	graph_button->SetTextSize(TEXT_SIZE);
 	graph_button->Draw();
@@ -103,7 +92,7 @@ void BB()
 	dissipazione_button->SetTextSize(TEXT_SIZE);
 	dissipazione_button->Draw();
 	
-	clear_button = new TButton("Cancella grafico", "pulisci()", 0, 0.3,1,0.25);
+	clear_button = new TButton("Cancella grafico", ")", 0, 0.3,1,0.25); //DA FARE
 	clear_button->SetTextSize(TEXT_SIZE);
 	clear_button->Draw();
 	
@@ -119,17 +108,7 @@ void exit_here()
         gROOT->ProcessLine(".q");
 }
 
-void pulisci()
-{
- /*     
- 	cout << "ripulisco eseguendo \ngROOT->pad1->Draw()\n";
-        	gROOT->pad1->Draw();
-*/
-}
-
-
-//Qui puoi selezionare il tuo proiettile e caricarne i valori
-void particle(int k)
+void particle(int k) //Qui puoi selezionare il tuo proiettile e caricarne i valori
 {
 	if (k == 0){
 		m = 9.38257*pow(10,8);
@@ -146,32 +125,26 @@ void particle(int k)
 		cout << "Muone selezionato." << endl;}
 }
 		
-//Qui puoi selezionare il bersaglio
-void atomic_number(int l)
+
+void atomic_number(int l) //Qui puoi selezionare il bersaglio
 {
 	if (l == 0) {
 		Z = 26;
 		A = 53.9396;
-		cout << "Ferro selezionato." << endl;
-		} 
+		cout << "Ferro selezionato." << endl;} 
 	if (l == 1) { 
 		Z = 79;
 		A = 196.9665;
-		cout << "Oro selezionato." << endl; 
-		}
+		cout << "Oro selezionato." << endl; }
 	if (l == 2) { 
 		Z = 47;
 		A = 106.9051;
-		cout << "Argento selezionato." << endl; 
-		}
+		cout << "Argento selezionato." << endl; }
 	if (l == 3){
 		Z = 13;
 		A = 26.9815;
-		cout << "Alluminio selezionato." << endl;
-		
-	}
+		cout << "Alluminio selezionato." << endl;}
 }
-
 
 double bethe_bloch(double beta) //Uno strumento che utilizzeremo molto spesso
 {
@@ -185,14 +158,16 @@ double bethe_bloch(double beta) //Uno strumento che utilizzeremo molto spesso
 	//restituisco il valore
 	return func;
 }
+
 void draw_graph() //Calcolo e plotto i valori di Bethe_Bloch
 {
-	double beta_min = betagamma_min/sqrt(1+pow(betagamma_min,2));
-	double beta_max = betagamma_max/sqrt(1+pow(betagamma_max,2));
+	double beta_min = BETAGAMMA_MIN/sqrt(1+pow(BETAGAMMA_MIN,2));
+	double beta_max = BETAGAMMA_MAX/sqrt(1+pow(BETAGAMMA_MAX,2));
 	TGraph* graph = new TGraph(); //creo un grafico
 	graph->SetTitle("Formula di Bethe-Bloch; #gamma*#beta; - #frac{dE}{dx} [MeV]"); //imposto il titolo del grafico e il nome degli assi
    	graph->GetXaxis()->CenterTitle(true);	
    	graph->GetYaxis()->CenterTitle(true);	
+	
 	//Assegno diversi colori in base al materiale del bersaglio
 	if (Z == 26){graph->SetLineColor(28);}
 	if (Z == 47){graph->SetLineColor(16);}
@@ -202,7 +177,7 @@ void draw_graph() //Calcolo e plotto i valori di Bethe_Bloch
 	else 
 	{
 		double x,y;
-		double delta_beta = (beta_max-beta_min)/incrementi;
+		double delta_beta = (beta_max-beta_min)/INCREMENTI;
 		long int i=0;
 		for (double beta = beta_min; beta < beta_max; beta = beta+delta_beta)
 		{
@@ -219,7 +194,7 @@ void draw_graph() //Calcolo e plotto i valori di Bethe_Bloch
 	}
 }
 		
-	void plot_dissipazione() //mostro il grafico della Stopping Power fino a quando la particella si arresta
+void plot_dissipazione() //mostro il grafico della Stopping Power fino a quando la particella si arresta
 {
 	if (Z == 0 || m == 0)
 	{
@@ -231,18 +206,21 @@ void draw_graph() //Calcolo e plotto i valori di Bethe_Bloch
 		graph->SetTitle("Dissipazione Energia; Spessore percorso dalla particella [cm]; Stopping Power #frac{[MeV]}{[cm]}");
 		graph->GetXaxis()->CenterTitle(true);	
    		graph->GetYaxis()->CenterTitle(true);
-		double x,inc, beta, gam, y,de;
+		double x, beta, gam, y,de;
 		long int i=0;
-		x=0;
+		x=0; //distanza percorsa nel bersaglio
 		beta=0.2; //fornisco il beta della particella
+		/*
+		TO DO: INSERISCI CASELLA DI TESTO CHE PRENDA IL VALORE DI BETA COME INPUT, VERIFICA SE MINORE DI UNO E MAGGIORE DI ZERO, E LO ASSEGNA ALLA VARIABILE
+		*/
 		gam = 1/sqrt(1-pow(beta,2)); //calcolo il corrispondente gamma
 		y=m*gam; //energia cinetica relativistica della particella prima che impatti contro il materiale
-		inc = 0.001 //spessore infinitesimo in cm;
+		inc = 0.001; //spessore infinitesimo in cm;
 		for(;y>=0;i++) //eseguo il ciclo finchè l'energia cinetica è 0
 		{
-			de=-bethe_bloch(beta)*inc; //perdita di energia infinitesima
+			de=-bethe_bloch(beta)*INC; //perdita di energia infinitesima
 			graph->SetPoint(i,x,-de*pow(10,-6)); //faccio il plot "spazio percorso vs energia dissipata"
-			x = x+inc; //mi sposto un po in avanti rispetto al punto di collisione
+			x = x+INC; //incremento la distanza percorsa nel bersaglio di un determinato spessore infinitesimo
 			y = y+de; //calcolo la perdita di energia cinetica (col segno più perchè de è negativa)
 			beta = sqrt(1-(pow(m/y,2))); //calcolo il nuovo beta corrispondente alla nuova energia
 			gam = 1/sqrt(1-(pow(beta,2))); //calcolo il nuovo gamma corrispondente al nuovo beta
@@ -252,4 +230,4 @@ void draw_graph() //Calcolo e plotto i valori di Bethe_Bloch
 		//gPad->SetLogx();
 		graph->Draw();
 	}
-}
+}	
